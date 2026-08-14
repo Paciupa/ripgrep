e9c5e5532c435ad14fb65ef58817b8c1a77b1038
Paciupa <65620103+Paciupa@users.noreply.github.com>
2026-08-14 19:52:56 +0300

    fix order

--- crates/ignore/src/default_types.rs
+++ crates/ignore/src/default_types.rs
@@ -213,12 +213,12 @@ pub(crate) const DEFAULT_TYPES: &[(&[&str], &[&str])] = &[
     (&["po"], &["*.po"]),
     (&["pod"], &["*.pod"]),
     (&["postscript"], &["*.eps", "*.ps"]),
-    (&["prolog"], &["*.pl", "*.pro", "*.prolog", "*.P"]),
-    (&["proto", "protobuf"], &["*.proto"]),
     (&["powershell", "ps", "pwsh"], &[
         "*.cdxml", "*.ps1", "*.ps1xml", "*.psc1", "*.psd1", "*.psm1", "*.psr",
          "*.psrc", "*.pssc", "*.psxml"
     ]),
+    (&["prolog"], &["*.pl", "*.pro", "*.prolog", "*.P"]),
+    (&["proto", "protobuf"], &["*.proto"]),
     (&["puppet"], &["*.epp", "*.erb", "*.pp", "*.rb"]),
     (&["purs"], &["*.purs"]),
     (&["py", "python"], &["*.py", "*.pyi"]),
